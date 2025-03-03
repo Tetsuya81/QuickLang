1f3ab731b9008bc610a06ba01ae1dcfac30e6213
Tetsuya81 <tetsuya8109@gmail.com>
2025-03-03 15:28:53 +0900

    Initial Commit

---
+++ QuickLang/QuickLangApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickLangApp.swift
+//  QuickLang
+//
+//  Created by Tokunaga Tetsuya on 2025/03/03.
+//
+
+import SwiftUI
+
+@main
+struct QuickLangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuickLangTests/QuickLangTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuickLangTests.swift
+//  QuickLangTests
+//
+//  Created by Tokunaga Tetsuya on 2025/03/03.
+//
+
+import Testing
+@testable import QuickLang
+
+struct QuickLangTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
